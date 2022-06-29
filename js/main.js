331ab50a6a3602ef9c0a4766f02cfd4b311a3152
ccmediadev <104095571+ccmediadev@users.noreply.github.com>
2022-06-29 18:53:58 -0400

    setup time function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 const timeElement = document.querySelector('.time')
 const dateElement = document.querySelector('.date')
+
+/**
+ * 
+ * @param {Date} date 
+ */
+const formatTime = (date) => {
+    const hours12 = date.getHours() % 12 || 12
+    const minutes = date.getMinutes()
+    const seconds = setInterval(() => {
+        date.getSeconds()
+    }, 1000)
+    const timeofDay = date.getHours() < 12
+    return `${hours12.toString()}:${minutes.toString().padStart(2, "0")}:${seconds} ${timeofDay ? "AN" : "PM"}`
+}
